856a814a66b5b385135dabdf30c7fc574455807c
JoaoPedroCavalcante <joaopedro.asc123@gmail.com>
2025-10-27 18:17:23 -0300

    Corrige verificação de erros em DB.AutoMigrate e r.Run; ajusta docker-compose para Postgres

--- database/db.go
+++ database/db.go
@@ -1,50 +1,34 @@
 package database
 
 import (
-	"log"
-	"os"
-	"time"
-	"fmt"
+    "log"
+    "os"
 
-	"github.com/guilhermeonrails/api-go-gin/models"
-	"gorm.io/driver/postgres"
-	"gorm.io/gorm"
+    "github.com/guilhermeonrails/api-go-gin/models"
+    "gorm.io/driver/postgres"
+    "gorm.io/gorm"
 )
 
 // ...existing code...
 var (
-	DB  *gorm.DB
-	err error
+    DB  *gorm.DB
+    err error
 )
 
 func ConectaComBancoDeDados() {
-	host := os.Getenv("DB_HOST")
-	user := os.Getenv("DB_USER")
-	password := os.Getenv("DB_PASSWORD")
-	dbname := os.Getenv("DB_NAME")
-	port := os.Getenv("DB_PORT")
-	dsn := fmt.Sprintf("host=%s user=%s password=%s dbname=%s port=%s sslmode=disable", host, user, password, dbname, port)
+    host  :=  os.Getenv("DB_HOST")
+    user  :=  os.Getenv("DB_USER")
+    password := os.Getenv("DB_PASSWORD")
+    dbname := os.Getenv("DB_NAME")
+    port := os.Getenv("DB_PORT")
+    stringDeConexao := "host=" + host + " user=" + user + " password=" + password + " dbname=" + dbname + " port=" + port + " sslmode=disable"
+    DB, err = gorm.Open(postgres.Open(stringDeConexao))
+    if err != nil {
+        log.Panic("Erro ao conectar com banco de dados")
+    }
 
-	
-	for attempt := 1; attempt <= maxAttempts; attempt++ {
-		DB, err = gorm.Open(postgres.Open(dsn))
-		if err == nil {
-			break
-		}
-		log.Printf("Tentativa %d/%d: erro ao conectar com banco: %v", attempt, maxAttempts, err)
-		if attempt == maxAttempts {
-			log.Panic("Erro ao conectar com banco de dados")
-		}
-		time.Sleep(3 * time.Second)
-	}
-
-	if DB == nil {
-		log.Panic("Erro ao conectar com banco de dados: DB nil após tentativas")
-	}
-
-	if err := DB.AutoMigrate(&models.Aluno{}); err != nil {
-		log.Printf("AutoMigrate error: %v", err)
-	}
+    if err := DB.AutoMigrate(&models.Aluno{}); err != nil {
+        log.Printf("AutoMigrate error: %v", err)
+    }
 }
-
-
+// ...existing code...
